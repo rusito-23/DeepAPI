293463deca2f30bb2bbd236abe32ca392d049f5e
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2020-07-10 17:37:18 -0300

    [algorithm]: class modularization

---
+++ deep_api/algorithm/googlenet.py
@@ -0,0 +1,54 @@
+"""
+GoogLeNet Model Extension.
+"""
+
+import torchvision
+from torch import nn
+from torch.hub import load_state_dict_from_url
+from utils.exception import ModelInitializationError
+
+WEIGHTS_URL = 'https://download.pytorch.org/models/googlenet-1378be20.pth'
+
+
+class DeepGoogLeNet(torchvision.models.GoogLeNet):
+    """
+    GoogLeNet Extension to save hooks for the Deep Dream Algorithm.
+    It will pass an input through a given number of layers and save
+    the partial outputs into the `features` array.
+    Parameters:
+        - loi:
+            Layers Of Interest - the first n layers to save hooks
+    """
+    features = []
+    hooks = []
+
+    def __init__(self, loi):
+        super(DeepGoogLeNet, self).__init__()
+        # load pretrained weights
+        state_dict = load_state_dict_from_url(WEIGHTS_URL, progress=False)
+        self.load_state_dict(state_dict)
+
+        # loi validation
+        if loi <= 0 or loi > 16:
+            raise ModelInitializationError()
+
+        # prepare layers
+        named_children = dict(self.named_children())
+        layer_names = list(named_children.keys())[:loi]
+        layers = [named_children[name] for name in layer_names]
+
+        # set layers and hooks
+        self.features = []
+        self.layers = nn.Sequential(*layers)
+        self.hooks = [layer.register_forward_hook(self.feature_hook)
+                      for layer in self.layers]
+
+    def feature_hook(self, module, _in, _out):
+        self.features.append(_out)
+
+    def remove(self):
+        [hook.remove() for hook in self.hooks]
+
+    def forward(self, x):
+        self.features = []
+        return self.layers(x)
---
+++ deep_api/algorithm/im_utils.py
@@ -0,0 +1,44 @@
+"""
+Image Utils for the Deep Dream Algorithm.
+"""
+
+import torch
+from torchvision import transforms as T
+
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+IMEAN = [-0.485/0.229, -0.456/0.224, -0.406/0.225]
+ISTD = [1/0.229, 1/0.224, 1/0.225]
+
+
+def preprocess(image):
+    """
+    Preprocesses the image to be fed to the Deep Model Algorithm.
+    Resizes, normalizes the image and performs Pytorch Tensor Conversion.
+    """
+    transforms = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean=MEAN, std=STD)
+    ])
+    return transforms(image).unsqueeze(0).requires_grad_(True)
+
+
+def postprocess(tensor):
+    """
+    Processes the output for the Deep Dream Algorithm.
+    """
+    transforms = T.Compose([
+        T.Normalize(IMEAN, ISTD),
+        T.ToPILImage()
+    ])
+    return transforms(tensor)
+
+
+def clip(image):
+    """
+    Set the image pixel values between 0 and 1.
+    """
+    for c, (mean, std) in enumerate(zip(MEAN, STD)):
+        image[0, c] = torch.clamp(image[0, c], -mean/std, (1 - mean)/std)
+    return image
+
